0f664916b985fe8f95a13899f7126af4b59a6bb5
costalapada <franciscocosta.py@gmail.com>
2022-10-14 10:52:18 +0100

    Schema atualizado

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,40 +1,38 @@
-create table Password (
-     id serial primary key,
-     password varchar(20)
-)
+
 
 create table "User" (
     id serial primary key,
     "name" varchar(20),
-    passwordID int,
-    foreign key(passwordID) references Password(id)
+    password varchar(20),
 )
 
 create table token(
     userID int,
-    token varchar(255),
+    token varchar(255) primary key,
     foreign key(userID) references "User"(id)
 );
 
 create table Board (
-    id serial primary key,
-    layout text
+    layout text,
+    gameId int foreign key references Game(id),
+    userId int foreign key references "User"(id),
+    primary key (gameId, userId)
 )
 
 create table FleetComposition (
-    id serial primary key,
+    fleetName varchar(20),
     shipSize int,
     numOfShips int,
+    primary key (fleetName, shipSize)
 )
-
 
 create table GameRules (
     id serial primary key,
     boardSide int,
     shotsPerTurn int,
-    maxTimeToDefineLayout int,
-    maxTimeToPlay int,
-    fleetComposition int, foreign key(fleetComposition) references FleetComposition(id)
+    layoutDefinitionTimeout int,
+    playTimeout int,
+    fleetComposition varchar(20), foreign key(fleetComposition) references FleetComposition(id)
 )
 
 create table Game (
@@ -43,20 +41,18 @@ create table Game (
     "state" varchar(20) check ( "state" like 'Placing' or "state" like 'Running' or "state" like 'Ended'),
     player1 int, foreign key(player1) references "User"(id),
     player2 int, foreign key(player2) references "User"(id),
-    boardP1 int, foreign key(boardP1) references Board(id),
-    boardP2 int, foreign key(boardP2) references Board(id),
     winner int, foreign key(winner) references "User"(id)
 )
 
 create table Authors(
-                        name varchar(20) primary key,
-                        email varchar(255) constraint email_invalid check(email ~* '^[A-Z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$')
+    name varchar(20) primary key,
+    email varchar(255) constraint email_invalid check(email ~* '^[A-Z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$')
     github varchar(255)
 )
 
 create table SystemInfo(
-                           name varchar(20) primary key,
-                           version varchar(20)
+       name varchar(20) primary key,
+       version varchar(20)
 )
 
 -------------------------------------------------------------------------------------------------------------------------
